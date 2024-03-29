d69b8c7f7c5f9021dd70ebc760546a5502b69753
Gekon <jakubocytko@student.agh.edu.pl>
2024-03-29 18:44:30 +0100

    delete all files

--- Assets/Scripts/PlayerBaseState.cs
+++
@@ -1,6 +0,0 @@
-public abstract class PlayerBaseState
-{
-    public abstract void Start(PlayerStateManager manager);
-    public abstract void Update(PlayerStateManager manager);
-    public abstract void FixUpdate(PlayerStateManager manager);
-}
